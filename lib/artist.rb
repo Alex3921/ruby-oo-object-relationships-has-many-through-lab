843e79de028c08d9d9c0693748e2e9c423fd290e
alexandru <boncut.alex10@gmail.com>
2021-03-23 10:16:40 -0500

    commit tree 770990db8b33d8271c1be8197d865d4bb6cfb586

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,29 @@
+class Artist
+    attr_accessor :genre
+    attr_reader :name, :song_name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(song_name, genre)
+        Song.new(song_name, self, genre)
+    end
+
+    def songs
+        Song.all.select{|song| song.artist == self}
+    end
+
+    def genres
+        self.songs.collect{|song| song.genre}
+    end
+
+end
 
